f952212c274e8c4edb2974c0c81154b3073bdf47
harsh <shivamkumarpaswan7488700578@gmail.com>
2025-02-25 03:01:44 +0530

    fixing deployment redirecting issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: [
-      '176a-2405-201-a410-b135-d430-7ceb-f555-7db0.ngrok-free.app', // Add your specific ngrok host here
-      'b8c8-2405-201-a410-b135-69f4-e646-8562-860.ngrok-free.app', // Add your specific ngrok host here
-    ],
+  base: '/',
+  build: {
+    outDir: 'dist'
   },
+  server: {
+    historyApiFallback: true
+  }
 });
