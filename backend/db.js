8d1e0211fe2c7061fbef18e3a6840462af2c417f
Hari Om <hari333333om@gmail.com>
2025-03-05 14:47:55 +0530

    Fixed Attendace page bug

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "harsh7488",
+  password: "Sql@12345",
   database: "attendance_system",
   waitForConnections: true,
   connectionLimit: 10,
